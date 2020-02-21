d614774579b243df90fbe54f1cc2e8a1412d9f09
Deepak Pandey <61325469+Deepak2302@users.noreply.github.com>
2020-02-21 22:45:47 +0530

    Update Snake-Gun-Water.py

--- Snake-Gun-Water.py
+++ Snake-Gun-Water.py
@@ -1,11 +1,13 @@
-# Snake_Gun_Water
+# Hi its Deepak Pandey again
+
 import random
 List = ("Gun","Water","Snake")
 j = 0
 m = 0
+n = 10
+player ={"g": "Gun","s":"Snake","w":"Water"}
 
-choice ={"g": "Gun","s":"Snake","w":"Water"}
-for i in range(9):
+for i in range(1,n+1):
     k = random.choice(List)
 
     Player_choice = input("Enter choice g for (gun), s for (water)or w for (snake):\n")
@@ -13,61 +15,80 @@ for i in range(9):
 
     if Player_choice == "g":
         print("Computer\'s choice: ", k)
-        print("Player\'s choice: ",choice[Player_choice])
+        print("Player\'s choice: ",player[Player_choice])
         if (Player_choice == "g") and (k == "Snake"):
             j = j+1
-            print("You Win", (j))
+            print("Your point", j)
             m = m-1
-            print("Opponent Loss", (m))
+            print("Opponent point", m)
+            n = n -1
+            print("Life Left", n)
         elif (Player_choice == "g") and (k == "Water"):
             j = j-1
-            print("You Loss", "Your Point:",(j))
+            print("Your Point: ",(j))
             m = m+1
-            print("Opponent Win","Opponent Points", (m))
+            print("Opponent Points: ", (m))
+            n = n - 1
+            print("Life Left", n)
         elif (Player_choice == "g") and (k == "Gun") and (i != 0):
             print("Your Score remain", (j))
             print("Opponent Score remain", (m))
+            n = n - 1
+            print("Life Left", n)
             print("Try Again")
+
         continue
 
 
 
     elif Player_choice == "s":
         print("Computer\'s choice: ", k)
-        print("Player\'s choice: ", choice[Player_choice])
+        print("Player\'s choice: ", player[Player_choice])
         if (Player_choice == "s") and (k == "Water"):
             j = j+1
-            print("You Win", (j))
+            print("Your Point: ", (j))
             m = m-1
-            print("Opponent Loss", (m))
+            print("Opponent Points: ", (m))
+            n = n - 1
+            print("Life Left", n)
         elif (Player_choice == "s") and (k == "Gun"):
             j = j-1
-            print("You Loss", (j))
+            print("Your Point: ", (j))
             m = m+1
-            print("Opponent Win", (m))
+            print("Opponent Points: ", (m))
+            n = n - 1
+            print("Life Left", n)
         elif (Player_choice == "s") and (k == "Snake") and (i != 0):
             print("Your Score remain", (j))
             print("Opponent Score remain", (m))
+            n = n - 1
+            print("Life Left", n)
             print("Try Again")
         continue
 
 
     elif Player_choice == "w":
         print("Computer\'s choice: ", k)
-        print("Player\'s choice: ", choice[Player_choice])
+        print("Player\'s choice: ", player[Player_choice])
         if (Player_choice == "w") and (k == "Gun"):
             j = j+1
-            print("You Win", (j))
+            print("Your Point: ", (j))
             m = m-1
-            print("Opponent Loss", (m))
+            print("Opponent Points: ", (m))
+            n = n - 1
+            print("Life Left", n)
         elif (Player_choice == "w") and (k == "Snake"):
             j = j-1
-            print("You Loss", (j))
+            print("Your Point: ", (j))
             m = m+1
-            print("Opponent Win", (m))
+            print("Opponent Points: ", (m))
+            n = n - 1
+            print("Life Left", n)
         elif (Player_choice == "w") and (k == "Water") and (i != 0):
             print("Your Score remain", (j))
             print("Opponent Score remain", (m))
+            n = n - 1
+            print("Life Left", n)
             print("Try Again")
         continue
 
@@ -75,8 +96,7 @@ for i in range(9):
 
 if j > m:
     print(":)Player Win With Point: ",j)
-elif m > j:
+elif m>j:
     print("*_*Computer Win With Point: ",m)
 else:
     print("Game Draw")
-
